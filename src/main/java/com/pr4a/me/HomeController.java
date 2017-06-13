30267edf7e39eb71e5ac49b2838257040bd82271
pr3a <5605104046@live4.utcc.ac.th>
2017-06-13 19:26:38 +0700

    Sprint Boot Tutorial

---
+++ src/main/java/com/pr4a/me/HomeController.java
@@ -0,0 +1,21 @@
+package com.pr4a.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!" + (8 + 7);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HomeController.class, args);
+    }
+}
